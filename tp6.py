ed349e33588ffb9ab2235e83c61df65b44e74605
AdnaneEmbouazza <adnaneembouazza77000@gmail.com>
2026-02-12 00:05:40 +0100

    ajout TP6

---
+++ tp6.py
@@ -0,0 +1,84 @@
+#question1
+
+#ticket resto
+# t=8.9
+# m=11
+# n=2018
+# while t<=m :
+#     t=t*1.05
+#     m=m+0.1
+#     n=n+1
+# print(n)
+
+
+# question2
+#Euclide
+#a=int(input("saisir le nbre le plus grd"))
+#b=int(input("saisir le nbre le plus petit"))
+#r=b
+#while r!=0 :
+    #r=a%b
+    #a=b
+    #b=r
+    #if r==1 :
+        #print(" les nombres sont premiers entre eux ")
+#print("le pgcd est",a)
+
+
+#Exercice 1
+#from random import*
+
+#mise=5
+#nblancer=0
+
+#while mise >0 :
+    #aleatoire = randint(0,1)
+
+    #if aleatoire==1:
+        #mise+=1
+
+    #else:
+        #mise-=1
+
+    #nblancer=nblancer+1
+
+#print ("le nbre de lancer est de",nblancer)
+
+#exercice 2
+
+#reponse="abc"
+#compteur=0
+
+#while(reponse!="sesame" and compteur<3):
+    #print("veuillez saisir le mot de passe")
+    #reponse=str(input())
+
+    #if reponse!="sesame":
+        #print("mot de passe incorrect \n")
+
+    #compteur=compteur+1
+
+#exercice 3
+
+#from random import*
+
+#codesecret=randint(1,100)
+#reponse=0
+#nbtentative=0
+#while(reponse!=codesecret):
+    #reponse=int(input("veuillez saisir un nombre"))
+
+    #if (reponse>codesecret) :
+        #print("essaie plus petit")
+
+    #elif (reponse<codesecret):
+        #print("essaie plus grand")
+
+    #nbtentative=nbtentative+1
+
+#print("le nombre de tentative est de" , nbtentative)
+
+#exercice 4
+
+
+
